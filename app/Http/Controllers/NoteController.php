93b5dae0cfc5467b3101d85a748d85eab2147759
Dallas Despain <dallas@Kaladin>
2024-08-04 15:42:34 -0600

    first commit

---
+++ app/Http/Controllers/NoteController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Note;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class NoteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $notes = Note::where('user_id', Auth::id())->latest('updated_at')->paginate(5);
+        return view('notes.index')->with('notes', $notes);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('notes.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' =>'required|max:120',
+            'text' => 'required',
+        ]);
+        Note::create([
+            'uuid' => Str::uuid(),
+            'user_id' => Auth::id(),
+            'title' => $request->title,
+            'text' => $request->text,
+        ]);
+        return to_route('notes.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Note $note)
+    {
+        //No longer needed since we are using route model binding $note = Note::where('uuid', $uuid)->where('user_id', Auth::id())->firstOrFail();
+        if($note->user_id != Auth::id()) {
+            return abort(403);
+        }
+        return view('notes.show')->with('note', $note);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
