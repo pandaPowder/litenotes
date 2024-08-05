db3ddc73a25bc979f05a96b35b945893245826f3
Dallas Despain <dallas@Kaladin>
2024-08-05 10:23:49 -0600

    editing notes is working

---
+++ resources/views/notes/edit.blade.php
@@ -0,0 +1,34 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Edit Note') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="p-6 text-gray-900">
+                <div class="my-6 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">
+                    
+                    <form action="{{ route('notes.update', $note) }}" method="post">
+                        @method('put')
+                        <label for="title">Title</label>
+                        <x-text-input
+                            type="text"
+                            name="title"
+                            field="title"
+                            id="title"
+                            placeholder="Be bold"
+                            class="w-full mb-6"
+                            :value="@old('title', $note->title)"></x-text-input>
+                        <label for="text" class="mt-6">Body</label>
+                        <x-textarea-input name="text" id="text" field="text" rows="10" placeholder="Start typing here..." class="w-full" :value="@old('text', $note->text)"></x-textarea-input>
+                        @csrf
+                        <br />
+                        <x-primary-button class="mt-6">Save Note</x-primary-button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
--- resources/views/notes/show.blade.php
+++ resources/views/notes/show.blade.php
@@ -14,6 +14,12 @@
                 <p class="opacity-70 ml-4">
                     <strong>Updated: {{ $note->updated_at->diffForHumans() }}</strong> 
                 </p>
+                <a href="{{ route('notes.edit', $note) }}" class="btn-link ml-auto">Edit Note</a>
+                <form action="{{ route('notes.destroy', $note) }}" method="post">
+                    @method('delete')
+                    @csrf
+                    <button type="submit" class="btn btn-danger ml-4" onclick="return confirm('Are you sure?');">Delete Note</button>
+                </form>
             </div>
             <div class="p-6 text-gray-900">
                 <div class="my-6 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">
